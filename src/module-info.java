e9a671eef9d4ca9cd19d88f203d60a9507cb1547
Warrick-Eddie <s3784777@student.rmit.edu.au>
2021-06-10 00:29:01 +1000

    First step in creating a JAVAFX application featuring a button that when clicked, opened up another window.

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Assignment.2.FP {
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.
-
-    opens
-        }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,64 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(final Stage primaryStage) {
+
+        Button button = new Button();
+        button.setText("Open a New Window");
+
+        button.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent actionEvent) {
+
+                Label secondLabel = new Label("me label");
+
+                StackPane secondaryLayout = new StackPane();
+                secondaryLayout.getChildren().add(secondLabel);
+
+                Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+                // New window (Stage)
+                Stage newWindow = new Stage();
+                newWindow.setTitle("Second Stage");
+                newWindow.setScene(secondScene);
+
+                // Specifies the modality for new window.
+                newWindow.initModality(Modality.WINDOW_MODAL);
+
+                // Specifies the owner Window (parent) for new window
+                newWindow.initOwner(primaryStage);
+
+                // Set position of second window, related to primary window.
+                newWindow.setX(primaryStage.getX() + 200);
+                newWindow.setY(primaryStage.getY() + 100);
+
+                newWindow.show();
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+
+        Scene scene = new Scene(root, 450, 250);
+
+        primaryStage.setTitle("this me new window");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
