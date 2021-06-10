298b466d1d43dcee06350c6ef499d631fe989747
Warrick-Eddie <s3784777@student.rmit.edu.au>
2021-06-11 02:56:39 +1000

    Still having trouble with the first implementation of database entry. Says that there is an exception in Application start method. And that InvocationTargetException, RuntimeException, Illegal StateException and the freaking start menu is bugged.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,22 +11,23 @@ import java.net.URL;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
 
         //Parent root = FXMLLoader.load(getClass().getResource("sample/FXML/Signup.fxml"));
-
-        URL url = Main.class.getResource("Signup.fxml");
-        Parent root = FXMLLoader.load(url);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Signup.fxml"));
+        Parent root = loader.load();
+        //URL url = Main.class.getResource("/FXML/Signup.fxml");
+        // URL url = getClass().getResource("Signup.fxml");
+        // Parent root = FXMLLoader.load(url);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 
 }
