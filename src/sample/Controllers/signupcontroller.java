9cc376c5c5ff2d1e01a0368ba209b03fc8c06314
Warrick-Eddie <s3784777@student.rmit.edu.au>
2021-06-10 23:04:03 +1000

    Configuring the controllers to make the database construction work, at the moment is the sign up controller and the execution of date within the login button

---
+++ src/sample/Controllers/signupcontroller.java
@@ -0,0 +1,35 @@
+package sample.Controllers;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class signupcontroller implements Initializable {
+
+
+    private TextField tf_id;
+    private TextField tf_first;
+    private TextField tf_last;
+    private TextField tf_role;
+    private TextField tf_user;
+    private PasswordField tf_pass;
+    private TextField tf_squestion;
+    private TextField tf_aquestion;
+
+    void login(MouseEvent event) {
+
+    }
+
+    void signup(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
